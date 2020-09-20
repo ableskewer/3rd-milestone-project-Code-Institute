b50ed22e25d1c2f71822824bd958ac451823c2ca
ableskewer <danse4@msn.com>
2020-09-20 21:52:32 +0000

    connect mongodb w python, db looped in index.html

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://ableskewer:dAnse*=>1337@cluster0.dmh5q.mongodb.net/third_milestone?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", listings=mongo.db.free_listings.find())
 
 
 @app.route("/post_items")
